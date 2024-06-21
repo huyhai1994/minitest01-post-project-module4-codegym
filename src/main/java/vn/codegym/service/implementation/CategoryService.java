2de67c4db9eb4d9e8963ff4680a808fc07ad82d2
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-06-21 21:33:13 +0700

    lenovo_huyhai1994 update CategoryService method

--- src/main/java/vn/codegym/service/implementation/CategoryService.java
+++ src/main/java/vn/codegym/service/implementation/CategoryService.java
@@ -1,29 +1,36 @@
 package vn.codegym.service.implementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import vn.codegym.model.Category;
+import vn.codegym.repository.ICategoryRepository;
 import vn.codegym.service.ICategoryService;
 
-import java.util.Collections;
 import java.util.List;
 
+@Service
 public class CategoryService implements ICategoryService {
+
+    @Autowired
+    private ICategoryRepository iCategoryRepository;
+
     @Override
     public List<Category> findAll() {
-        return ;
+        return iCategoryRepository.findAll();
     }
 
     @Override
     public void save(Category category) {
-
+        iCategoryRepository.save(category);
     }
 
     @Override
     public Category findById(Long id) {
-        return null;
+        return iCategoryRepository.findById(id);
     }
 
     @Override
     public void remove(Long id) {
-
+        iCategoryRepository.remove(id);
     }
 }
