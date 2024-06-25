43d813650188e51e58a21cd059260b3d01a278ad
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-06-25 17:19:39 +0700

    lenovo_huyhai1994 fixed validation for create(posts)

--- src/main/java/vn/codegym/model/posts/PostsDTO.java
+++ src/main/java/vn/codegym/model/posts/PostsDTO.java
@@ -3,18 +3,17 @@ package vn.codegym.model.posts;
 import org.springframework.web.multipart.MultipartFile;
 import vn.codegym.model.category.Category;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.NotNull;
 
 public class PostsDTO {
     @NotEmpty(message = "Khong duoc de trong ten bai viet")
     private String title;
 
-    @NotEmpty(message = "Khong duoc de trong noi dung")
+    @NotBlank(message = "Khong duoc de trong noi dung")
     private String content;
-
-    @Size(min = 10, message = "Mo ta nen it nhat 10 ki tu")
-    @Size(max = 2000, message = "Mo ta khong duoc qua 2000 ki tu")
+    @NotNull(message = "Khong duoc de trong")
     private String shortDescription;
 
     private MultipartFile imageFile;
