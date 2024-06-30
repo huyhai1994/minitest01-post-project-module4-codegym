cb2eae6c38a9daac9c9c4ef4f6217dda79688eb8
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-06-30 07:11:05 +0700

    lenovo_huyhai1994 update ajax(display images)

--- ajax/index.js
+++ ajax/index.js
@@ -2,18 +2,26 @@ function showAllPosts() {
   $.ajax({
     type: 'GET',
     url: '//localhost:8080/api/posts',
-    success: function (data) {
+    success: (data) => {
       let content = '';
+      let localHost = 'http://127.0.0.1:5500/';
+      let imageAbsolutePath =
+        'Module4CodeGym/post-minitest-springmvc/storage/images/';
+
       const dummyObjectWhenCategoryNull = { name: 'khong co' };
       for (let index = 0; index < data.length; index++) {
         if (data[index].category === null)
           data[index].category = dummyObjectWhenCategoryNull;
+        console.log(data[index].imageFileName);
         content += `  <tr>
                 <td>${index + 1}</td>
                 <td>${data[index].title}</td>
                 <td>${data[index].content}</td>
                 <td>${data[index].shortDescription}</td>
                 <td>${data[index].category.name}</td>
+                <td><img src=${localHost}${imageAbsolutePath}${
+          data[index].imageFileName
+        } width="100px" alt=""></td>
             </tr>`;
         console.log(data[index].category);
       }
